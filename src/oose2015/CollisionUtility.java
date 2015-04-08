f96ac03c40b1c424d16e6b8628cbedc909b56afc
Itai Yavin <Itai.yavin@gmail.com>
2015-04-08 16:56:46 +0200

    Created CollisionUtility class

---
+++ src/oose2015/CollisionUtility.java
@@ -0,0 +1,13 @@
+package oose2015;
+
+import org.newdawn.slick.geom.Vector2f;
+
+import oose2015.entities.Entity;
+
+public class CollisionUtility {
+
+	
+	public void CollisionCheck(Entity a, Entity b){
+
+	}
+}
