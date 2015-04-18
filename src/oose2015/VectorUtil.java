2b3c6c5658360d33af5f18e525e1b1a6b9ce7698
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2015-04-18 12:52:08 +0200

    described vector utility function

--- src/oose2015/VectorUtil.java
+++ src/oose2015/VectorUtil.java
@@ -17,6 +17,12 @@ import org.newdawn.slick.geom.Vector2f;
 public class VectorUtil {
 
 
+    /**
+     * Gets the distance from one Entity to another. NOT DISTANCE FROM CENTER TO CENTER but from circle edge to circle edge!
+     * @param entity Entity
+     * @param other other Entity
+     * @return distance between Entities
+     */
     public static float getDistanceToEntity(Entity entity, Entity other){
         Vector2f delta = other.position.copy().sub(entity.position);
         return delta.length() - entity.size.x/2 - other.size.x/2;
