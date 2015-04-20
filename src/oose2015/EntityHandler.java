c67e2115e371ddcf1e031a29761c58b87329ca48
Itai Yavin <Itai.yavin@gmail.com>
2015-04-20 10:32:29 +0200

    Fixed merge mistake from last push.

--- src/oose2015/EntityHandler.java
+++ src/oose2015/EntityHandler.java
@@ -2,8 +2,8 @@ package oose2015;
 
 import oose2015.entities.Enemy;
 import oose2015.entities.Entity;
+import oose2015.entities.MovableEntity;
 import oose2015.entities.Player;
-import oose2015.entities.Projectile;
 
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
@@ -37,38 +37,36 @@ public class EntityHandler {
         enemies = new ArrayList<Enemy>();
         players = new ArrayList<Player>();
 
-        Player p = new Player(new Vector2f(10,10), Input.KEY_UP, Input.KEY_DOWN, Input.KEY_LEFT, Input.KEY_RIGHT, Input.KEY_SPACE, Input.KEY_LALT );
+        Player p = new Player(new Vector2f(10,10), Input.KEY_UP, Input.KEY_DOWN, Input.KEY_LEFT, Input.KEY_RIGHT, Input.KEY_SPACE, Input.KEY_LCONTROL);
         input.addKeyListener(p);
-        players.add(p);
 
-        enemies.add(new Enemy(new Vector2f(0,0)));
-
-
+        new Enemy(new Vector2f(0,0));
     }
 
-    public void update(int dt){
-        for (Entity player : players) {
-            player.update(dt);
+    public void update(float dt){
+        for (int i = 0; i < entities.size(); i++) {
+            Entity entity = entities.get(i);
+            entity.update(dt);
+
         }
-        for (Entity enemy : enemies) {
-            enemy.update(dt);
-        }
-        for (Entity projectile : projectiles){
-        	projectile.update(dt);
+    }
+
+    public void updatePhysics(float dt){
+
+        //Collision
+        for (int i = 0; i < entities.size(); i++) {
+            Entity entity = entities.get(i);
+            for (int j = i+1; j < entities.size(); j++) {
+                Entity other = entities.get(j);
+
+                CollisionUtil.handleCollision(entity,other);
+            }
         }
     }
 
     public void render(Graphics graphics){
-        for (Entity enemy : enemies) {
-            enemy.render(graphics);
-        }
-
-        for (Entity player : players) {
-            player.render(graphics);
-        }
-        
-        for (Entity projectile : projectiles){
-        	projectile.render(graphics);
+        for(Entity entity : entities){
+            entity.render(graphics);
         }
     }
 
