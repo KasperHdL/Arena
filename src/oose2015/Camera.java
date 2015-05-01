5b72a9ffa653e2140d952301da0ef7ca28813cf1
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2015-05-01 14:42:52 +0200

    Camera is now centered of all players

--- src/oose2015/Camera.java
+++ src/oose2015/Camera.java
@@ -2,9 +2,8 @@ package oose2015;
 
 import oose2015.entities.Entity;
 import oose2015.entities.tiles.Tile;
+import org.newdawn.slick.geom.Polygon;
 import org.newdawn.slick.geom.Vector2f;
-
-import java.util.Vector;
 
 /**
  * Created by @Kasper on 01/05/2015
@@ -21,26 +20,35 @@ public class Camera {
     public Vector2f position;
     public Vector2f halfViewSize;
 
+    private float halfTileSize;
+
     public Camera(){
         halfViewSize = new Vector2f(Main.SCREEN_WIDTH/2,Main.SCREEN_HEIGHT/2);
+        halfTileSize = Tile.TILE_SIZE/2;
     }
 
     public void update(float dt){
-        position = World.PLAYERS.get(0).position;
+        Polygon polygon = new Polygon();
+        for (int i = 0; i < World.PLAYERS.size(); i++) {
+            Vector2f pos = World.PLAYERS.get(i).position;
+            polygon.addPoint(pos.x,pos.y);
+        }
+        position = new Vector2f(polygon.getCenterX(),polygon.getCenterY());
+
     }
 
     public boolean entityWithinView(Entity entity){
-        return  entity.position.x > position.x - halfViewSize.x &&
-                entity.position.x < position.x + halfViewSize.x &&
-                entity.position.y > position.y - halfViewSize.y &&
-                entity.position.y < position.y + halfViewSize.y;
+        return  entity.position.x + entity.size.x/2 > position.x - halfViewSize.x &&
+                entity.position.x - entity.size.x/2 < position.x + halfViewSize.x &&
+                entity.position.y + entity.size.x/2 > position.y - halfViewSize.y &&
+                entity.position.y - entity.size.x/2 < position.y + halfViewSize.y;
     }
     
     public boolean tileWithinView(Tile tile){
-        return  tile.position.x > position.x - halfViewSize.x &&
-                tile.position.x < position.x + halfViewSize.x &&
-                tile.position.y > position.y - halfViewSize.y &&
-                tile.position.y < position.y + halfViewSize.y;
+        return  tile.position.x + halfTileSize > position.x - halfViewSize.x &&
+                tile.position.x - halfTileSize < position.x + halfViewSize.x &&
+                tile.position.y + halfTileSize > position.y - halfViewSize.y &&
+                tile.position.y - halfTileSize < position.y + halfViewSize.y;
     }
 
 }
