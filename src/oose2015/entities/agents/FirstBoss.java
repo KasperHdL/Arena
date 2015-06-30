1312ea23e7de7396e2e83aed0b37b9a641df0395
KasperHdL <kasper@honnens.dk>
2015-06-30 17:26:24 +0200

    Merge

--- src/oose2015/entities/agents/FirstBoss.java
+++ src/oose2015/entities/agents/FirstBoss.java
@@ -2,120 +2,102 @@ package oose2015.entities.agents;
 
 import oose2015.Assets;
 import oose2015.World;
-import oose2015.utilities.CollisionUtility;
 import oose2015.utilities.VectorUtility;
-
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Sound;
 import org.newdawn.slick.geom.Vector2f;
 
 public class FirstBoss extends BossEnemy{
-	
+
+	protected MoveState movement = MoveState.RIGHTHAND;
 	FirstBossHand 	rightHand,
 					leftHand,
 					previousHand;
 	Sound roar = Assets.SOUND_FIRST_BOSS_ROAR;
-	
 	Vector2f currentHandPosition = new Vector2f();
-	
 	boolean hasInitiallyRoared = false;
 	boolean isRoaring = false,
 			isJumping = false,
 			isPunching = false;
-	
 	boolean rightHandMoved = false;
 	boolean leftHandMoved = false;
 	boolean reachedHand = false;
-	
 	float attackRange;
-	
 	float[] playerDamages;
-	
-	protected enum MoveState{
-		RIGHTHAND,
-		LEFTHAND,
-		BODY
-	}
-	
-    protected MoveState movement = MoveState.RIGHTHAND;
 	
 	public FirstBoss(Vector2f position, int level) {
 		super(position, level);
-		// TODO Auto-generated constructor stub
 		name = "Ogre";
         size = new Vector2f(100f, 100f);
         rightHand = new FirstBossHand(new Vector2f(position.x, position.y-size.y/2), this);
         leftHand = new FirstBossHand(new Vector2f(position.x, position.y+size.y/2), this);
 	}
-	
+
 	@Override
-	public void update(float dt){
+	public void update(float dt) {
 		System.out.println(movement);
-		// TODO Auto-generated method stub
-		if(target != null){
+		if (target != null) {
 			rightHand.update(dt);
 			leftHand.update(dt);
 		}
-		
-		if(getClosestPlayer().position.distance(position) < engageRadius && target == null){
+
+		if (getClosestPlayer().position.distance(position) < engageRadius && target == null) {
 			target = getClosestPlayer();
 			rightHand.getTarget(target);
 			leftHand.getTarget(target);
 		}
-		
-		if(target != null){
+
+		if (target != null) {
 			float dist = VectorUtility.getDistanceToEntity(this, target);
-			
-			if(!hasInitiallyRoared){
+
+			if (!hasInitiallyRoared) {
 				roar();
-		    	hasInitiallyRoared = true;
+				hasInitiallyRoared = true;
 			}
-			
-			switch(movement){
+
+			switch (movement) {
 				case RIGHTHAND:
-					if(!rightHand.gotPosition)
+					if (!rightHand.gotPosition)
 						rightHand.calculateNextPosition();
 					break;
 				case LEFTHAND:
-					if(!leftHand.gotPosition)
+					if (!leftHand.gotPosition)
 						leftHand.calculateNextPosition();
 					break;
 				case BODY:
 					moveToHand(previousHand, dt);
 					float distanceFromHand = VectorUtility.getDistanceToEntity(this, previousHand);
-					if(distanceFromHand < 10)
-						if(previousHand == rightHand)
+					if (distanceFromHand < 10)
+						if (previousHand == rightHand)
 							movement = MoveState.LEFTHAND;
 						else
 							movement = MoveState.RIGHTHAND;
 					break;
 			}
-			
+
 		}
 	}
-
+	
 	@Override
 	public void render(Graphics graphics) {
-		// TODO Auto-generated method stub
         graphics.pushTransform();
         graphics.translate(position.x, position.y);
         graphics.rotate(0, 0, rotation);
-        
+
         if(isAlive){
         	graphics.setColor(Color.black);
         	graphics.fillOval(-size.x/2, -size.y/2, size.x, size.y);
         	graphics.setColor(Color.red);
         	graphics.drawLine(0,0,size.x/2,0);
-        	
+
             graphics.popTransform();
         }
 	}
-	
+
 	protected void moveToHand(FirstBossHand hand, float dt){
-		Vector2f delta = new Vector2f();
-		delta = hand.position.copy().sub(position);
-		
+		Vector2f delta = hand.position.copy().sub(position);
+
 		move(dt, delta);
 	}
 	
@@ -128,27 +110,26 @@ public class FirstBoss extends BossEnemy{
 
         super.move(dt, input);
     }
-    
-    protected void roar(){
+
+	protected void roar(){
     	roar.play();
     	World.camera.shakeScreen(new Vector2f(World.RANDOM.nextFloat()*30,World.RANDOM.nextFloat()*30),2000,4f);
     }
-	
+
 	/**
-	 * @param targetPosition
-	 * rapidly moves boss towards target position
-	 * If collided with target at destination, target needs to take damage
+	 * @param targetPosition rapidly moves boss towards target position
+	 *                       If collided with target at destination, target needs to take damage
 	 */
-	protected void jump(Vector2f targetPosition){
-		
+	protected void jump(Vector2f targetPosition) {
+
 	}
-	
+
 	/**
 	 * @param minionAmount
 	 * Spawns minions corresponding to integer size.
 	 */
 	protected void spawnMinions(int minionAmount){
-		
+
 	}
 	
 	/**
@@ -157,8 +138,14 @@ public class FirstBoss extends BossEnemy{
 	 * moves hand to target position
 	 * during move if collided with target player
 	 */
-	protected void punch(FirstBossHand hand, Vector2f targetPosition){
-		
+	protected void punch(FirstBossHand hand, Vector2f targetPosition) {
+
+	}
+
+	protected enum MoveState {
+		RIGHTHAND,
+		LEFTHAND,
+		BODY
 	}
 	
 }
